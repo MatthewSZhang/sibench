e824f2d726e5cf0f7403ffa147643a010220247c
sikai zhang <matthew.szhang91@gmail.com>
2026-01-21 20:25:45 +0800

    MNT source venv

--- run_hpopt.sh
+++ run_hpopt.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+source .venv/bin/activate
+
 datasets=("EMPS" "CED" "WienerHammerBenchMark" "Silverbox" "F16" "ParWH" "Cascaded_Tanks")
 
 mkdir -p logs
 
 for data in "${datasets[@]}"; do
     echo "Starting process for: $data"
-    uv run opt-pysindy --data "$data" > "logs/pysindy_${data}.log" 2>&1 &
-    uv run opt-fastcan --data "$data" > "logs/fastcan_${data}.log" 2>&1 &
+    opt-pysindy --data "$data" > "logs/pysindy_${data}.log" 2>&1 &
+    opt-fastcan --data "$data" > "logs/fastcan_${data}.log" 2>&1 &
 done
 
 wait
