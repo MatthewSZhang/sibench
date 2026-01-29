82f4b6df43635b7f7428b00e8aa34514603a1a16
sikai zhang <matthew.szhang91@gmail.com>
2026-01-29 14:44:20 +0800

    MNT make a fair comparison bewteen pysindy and fastcan

--- run_hpopt.sh
+++ run_hpopt.sh
@@ -8,8 +8,8 @@ mkdir -p logs
 
 for data in "${datasets[@]}"; do
     echo "Starting process for: $data"
-    uv run opt-pysindy --data "$data" --n-folds 1 > "logs/pysindy_${data}.log" 2>&1 &
-    uv run opt-fastcan --data "$data" --n-folds 1 > "logs/fastcan_${data}.log" 2>&1 &
+    uv run opt-pysindy --data "$data" --n-folds 1 --n-trials 100 > "logs/pysindy_${data}.log" 2>&1 &
+    uv run opt-fastcan --data "$data" --n-folds 1 --n-trials 100 > "logs/fastcan_${data}.log" 2>&1 &
 done
 
 wait
